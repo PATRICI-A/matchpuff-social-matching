d31e71c8cbf291bbe8b55c2b32a07ad5a31e95cf
Andrew7480 <andrewc7480@gmail.com>
2026-05-13 23:43:24 -0500

    fixing deploy

--- src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/config/ProdSecurityConfig.java
+++ src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/config/ProdSecurityConfig.java
@@ -26,7 +26,8 @@ public class ProdSecurityConfig {
                     "/actuator/health",
                     "/swagger-ui/**",
                     "/swagger-ui.html",
-                    "/v3/api-docs/**"
+                    "/v3/api-docs/**",
+                    "/api/v1/categories/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             );
