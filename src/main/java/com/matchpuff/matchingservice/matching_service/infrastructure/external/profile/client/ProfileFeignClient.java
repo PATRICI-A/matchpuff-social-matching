8aad023d5b16fbbf9f34f0fea606c9b2799b541f
Andrew7480 <andrewc7480@gmail.com>
2026-05-14 19:46:59 -0500

    adding feign comunication with profile service

--- src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/external/profile/client/ProfileFeignClient.java
+++ src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/external/profile/client/ProfileFeignClient.java
@@ -5,8 +5,11 @@ import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.matchpuff.matchingservice.matching_service.infrastructure.external.profile.dto.FriendRequestDto;
 import com.matchpuff.matchingservice.matching_service.infrastructure.external.profile.dto.UserMatchProfileDto;
 
 @FeignClient(
@@ -21,4 +24,7 @@ public interface ProfileFeignClient {
 
     @GetMapping("/matching/profiles")
     List<UserMatchProfileDto> getAllProfiles();
+
+    @PatchMapping("/users/{userId}/friends")
+    void addFriend(@PathVariable UUID userId, @RequestBody FriendRequestDto request);
 }
