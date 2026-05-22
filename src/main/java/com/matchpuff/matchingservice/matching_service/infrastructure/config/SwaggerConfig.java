651bf91082e458fc6a16456e4400bf315b4f876c
Andrew7480 <andrewc7480@gmail.com>
2026-05-22 03:23:34 -0500

    adding bearer auth

--- src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/matchpuff/matchingservice/matching_service/infrastructure/config/SwaggerConfig.java
@@ -4,14 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String BEARER_AUTH = "bearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+        .components(new Components().addSecuritySchemes(BEARER_AUTH,
+            new SecurityScheme()
+                .name(BEARER_AUTH)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Matching Service API")
                         .version("1.0.0")
